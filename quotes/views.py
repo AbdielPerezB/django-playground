629aae790bac7aea10cd9fe562fbfbec627d94f0
Abdiel Pérez <abdiel2012p@gmail.com>
2026-02-14 19:38:27 -0600

    fin section 1

--- quotes/views.py
+++ quotes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
-
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 days_of_week = {
@@ -14,8 +14,27 @@ days_of_week = {
 }
 
 
+def index(request):
+    list_items = ""
+    days = list(days_of_week.keys())
+    
+    for day in days:
+        day_path = reverse("day-quote", args=day)
+        list_items += f'<li><a href="{day_path}">{day}</a></li>'
+        
+    response_html = f'<ul>{list_items}</ul>'
+    return HttpResponse(response_html)
+
+
 def days_week_with_number(request, day):
-    return HttpResponse(day)
+    days = list(days_of_week.keys())
+    if day > len(days):
+        return HttpResponseNotFound("El día no existe")
+    redirect_days = days[day-1]
+    # EL argumento de <str:day>
+    redirect_path = reverse("day-quote", args=[redirect_days])
+    # return HttpResponseRedirect(f"/quotes/{redirect_days}") #Sin reverse
+    return HttpResponseRedirect(redirect_path)  # Con reverse
 
 
 def days_week(request, day):
